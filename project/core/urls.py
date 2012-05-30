0960d1578855494c0c6efd8f0af8e57a4b02f9c5
Isaac Bythewood <isaac@bythewood.me>
2012-05-30 23:12:10 +0000

    New design stated.

--- project/core/urls.py
+++ project/core/urls.py
@@ -3,7 +3,6 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns('',
     url(r'^$', 'project.core.views.home', name='home'),
-    url(r'^pick/$', 'project.core.views.pick', name='pick'),
     url(r'^like/(?P<media_id>[0-9_]+)/$', 'project.core.views.like', name='like'),
     url(r'^oauth_callback/$', 'project.core.views.oauth_callback', name='oauth-callback'),
     url(r'^oauth_logout/$', 'project.core.views.oauth_logout', name='oauth-logout'),
--- project/core/views.py
+++ project/core/views.py
@@ -11,64 +11,52 @@ unauthenticated_api = client.InstagramAPI(**settings.INSTAGRAM_CONFIG)
 
 
 def home(request):
-    if request.session.get('access_token'):
-        return HttpResponseRedirect(reverse('core:pick'))
+    access_token = request.session.get('access_token')
+    instagram_oauth_url = None
 
-    instagram_oauth_url = unauthenticated_api.get_authorize_url(
-        scope=["likes"])
+    if access_token:
+        api = client.InstagramAPI(access_token=access_token[0])
+        popular = api.media_popular(count=2)
+    else:
+        instagram_oauth_url = unauthenticated_api.get_authorize_url(
+            scope=["likes"])
+
+        api = client.InstagramAPI(
+            client_id=settings.INSTAGRAM_CONFIG['client_id'],
+            client_secret=settings.INSTAGRAM_CONFIG['client_secret'])
+        popular = api.media_popular(count=2)
+
     return TemplateResponse(request, 'core/home.html', {
         'instagram_oauth_url': instagram_oauth_url,
-    })
-
-
-def pick(request):
-    access_token = request.session.get('access_token')
-    if not access_token:
-        return HttpResponseRedirect(reverse('core:home'))
-
-    api = client.InstagramAPI(access_token=access_token[0])
-    popular = api.media_popular(count=2)
-
-    return TemplateResponse(request, 'core/pick.html', {
-        'popular': popular,
         'access_token': access_token,
-        'counter': request.session.get('counter'),
+        'popular': popular,
+        'previous_vote': None,
     })
 
 
 def like(request, media_id):
     access_token = request.session.get('access_token')
     if not access_token:
-        return HttpResponseRedirect(reverse('core:home'))
-
-    api = client.InstagramAPI(access_token=access_token[0])
-    api.like_media(media_id)
-    if request.session.get('counter'):
-        request.session['counter'] += 1
+        messages.error(request, 'Please login with Instagram before voting!')
     else:
-        request.session['counter'] = 1
+        api = client.InstagramAPI(access_token=access_token[0])
+        api.like_media(media_id)
     
-    return HttpResponseRedirect(reverse('core:pick'))
+    return HttpResponseRedirect(reverse('core:home'))
 
 
 def oauth_callback(request):
-    error = False
     code = request.GET.get('code')
     if code:
         access_token = unauthenticated_api.exchange_code_for_access_token(code)
         request.session['access_token'] = access_token
         if not access_token:
-            error = True
             messages.error(request, 'Could not get access token.')
     else:
-        error = True
         messages.error(request, 'You must have an oauth access code to login, '
-                               'no code was provided.')
+                                'no code was provided.')
 
-    if error:
-        return HttpResponseRedirect(reverse('core:home'))
-    else:
-        return HttpResponseRedirect(reverse('core:pick'))
+    return HttpResponseRedirect(reverse('core:home'))
 
 
 def oauth_logout(request):
